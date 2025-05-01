1a94093ead02d6bb2ac520db264af540cac0b537
Sanket Gaikwad <178196375+SanketG-7@users.noreply.github.com>
2025-05-01 23:31:28 +0530

    Create cal.java

---
+++ cal.java
@@ -0,0 +1,19 @@
+package in.bank.util;
+
+import in.bank.geometry.* ;
+
+
+public class calculator {
+
+    public static void main(String[] args) {
+        circle   cir=new circle(5.5);
+        Rectangle react=new Rectangle(10,5);
+        double cirarea=Math.PI *Math.pow(cir.radius,2);
+        double reacArea = react.breadth*react.length;
+
+        System.out.printf("Area of the circle is=%d",cirarea,reacArea);
+
+
+    }
+
+}
